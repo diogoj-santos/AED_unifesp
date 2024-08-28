14d49f4164f86f26817b853454ead465a4c7f7c7
Diogo Santos <diogo.santos06@unifesp.br>
2024-08-28 13:33:40 -0300

    1003

---
+++ output/1003.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+  int main(void) {
+  int nmr1, nmr2, SOMA;
+  
+  scanf("%d", &nmr1);
+  scanf("%d", &nmr2);
+    SOMA = nmr1 + nmr2;
+  printf("SOMA = %d\n", SOMA);
+  return 0;
+}
